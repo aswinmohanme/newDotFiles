93ce437c76ea83801762a9e1e9fc7b302abc4186
aswinmohanme <git.aswinmohan@gmail.com>
2018-05-26 18:50:15 +0530

    Add Vim Keybindings to the Fish Prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,11 @@ set -gx EDITOR nvim
 # FZF
 set -g -x FZF_DEFAULT_COMMAND 'ag --ignore-case --depth -1 -g ""'
 set -g -x FZF_CTRL_T_COMMAND 'ag --ignore-case --depth -1 -g ""'
+
+# VIM
+# note: this erases the default fish_mode_prompt, which adds a vi mode
+# indicator to the left of your prompt
+function fish_mode_prompt; end
+
+# turn on vi mode when the shell starts
+fish_vi_key_bindings
